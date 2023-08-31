377df57ea1cc202214f6ede524e9ed838afa02c6
boykolumbev <boiko.lumbev@gmail.com>
2023-08-31 20:52:03 +0300

    hi

---
+++ lab3/lab3.py
@@ -0,0 +1,69 @@
+# #multilain string
+# print("""      
+#  hi
+# """) 
+# #immutable - nepromenqem
+# user_name = 'ivan'
+# user_name.upper()
+# x=user_name.upper()
+# print(x)  #IVAN
+# print(user_name) #ivan
+
+
+# #mutable - promenqem
+
+
+
+#Escape Sequences
+# sentence = 'Tom\'s pub is '
+
+
+
+# #Concatenation
+# user_name = 'Ada'
+# user_sirname = 'Byron'
+# print(user_name + ' ' +user_sirname)
+# print(3+3) #int
+# print('3'+'3') #String
+
+
+
+
+# #Repetition 
+# print(30 * '~')
+
+
+# #String Formating
+
+
+
+# #apple  
+# #orange 
+# fruit = input('Enter fruit name')
+# #fruit_formated = "'{}'".format(fruit)
+# #print(fruit_formated)
+# #print('|{:<10}|'.format(fruit)) #|apple     |
+# print('|{:<10}|'.format(fruit))  # TODO: ask chatGPT
+
+
+
+
+
+# user_name='Ada'
+# user_sirname = 'Byron'
+# print(f'Hello, {user_name} {user_sirname}!')
+
+
+# #.replace()
+# str1 = 'ala bala a b c'
+# #replace ala with @ in str1
+# str1 = str1.replace('ala', '@', 1 )
+# print(str1)
+
+# #  find()
+# user_name = input('Enter name')
+# user_name.find('a')
+
+
+
+
---
+++ lab4/conditional_statements.py
@@ -0,0 +1,40 @@
+# #expression - calculate to valiue
+# #Statement - do something
+# #user_age = 21
+# #if user_age >=18:
+# #    print('Welcome')
+# #    print('Not welcome')
+
+
+
+# #if x is even => print('Even')
+# #if x is odd => print("Odd")
+
+# x=6
+
+# if x%2==0:
+#     print("Even")
+# elif x==0:
+#     print("Zero")
+# else:
+#     print("Odd")
+# #ctr + '+' increase font size
+# #Pass statement - programata propuska reda
+# user_age=12
+# if user_age>=18:
+#     pass
+#     print("text")
+
+
+# #Ternery operator:
+
+
+# #user_age = 30
+# #status = ' '
+# #if user_age>=18:
+# #    status='Adult'
+# #else:
+#  #
+#  #   status='Child'
+
+# user_age =       
