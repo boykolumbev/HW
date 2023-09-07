265ec8d943e9685ffbb803d6f58fbeff6d0b6c7a
boykolumbev <boiko.lumbev@gmail.com>
2023-09-07 21:01:58 +0300

    s

--- lab8/lab8.py
+++ lab8/lab8.py
@@ -15,3 +15,38 @@ user_data_LOD  = [
     {'name':'Ivan' , 'score': 3},
 ]
 print(f"{user_data_LOD[2]['name']} -{user_data_LOD[2]['score']} ")
+#d1 = {}   d1=dict()   prazni rechnici
+
+
+
+# Dict metodi i operacii
+
+#d = {'age': 34}
+#d['name'] = 'Maria'
+#update value by
+#d['age']=99
+#print(d)
+
+
+
+#Remove element by key
+
+d={'age':32, 'name': 'Maria'}
+age=d.pop('age')
+print(d)
+print('age')
+
+
+#Copy dicionarys
+
+
+user_data={'age':32, 'name': 'Maria'}
+#ne e kopie, rechnika ima 2 imena
+#user_data_copy={'age':32, 'name': 'Maria'}
+
+user_data_copy = user_data.copy()
+
+
+
+
+
